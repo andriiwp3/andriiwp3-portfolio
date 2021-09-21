9b7ec6a923568c13108dc797defb5153a46f1e51
andriiwp3 <andriiwp3@gmail.com>
2021-09-21 21:01:27 +0300

    Fixed bug with not defined function

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -30,7 +30,6 @@ export default function ContactForm() {
       <button
         type="submit"
         className="bg-blue-500 rounded-md w-1/2 mx-4 mt-8 py-2 text-gray-50 text-xs font-bold"
-        onSubmit={handleSubmit}
       >
         Отправить сообщение
       </button>
