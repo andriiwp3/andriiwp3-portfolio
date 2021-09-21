b25b1e48805346d2d5cb0830a167d06cf1bbbae1
andriiwp3 <andriiwp3@gmail.com>
2021-09-21 20:51:17 +0300

    Made an experience page

---
+++ pages/experience.js
@@ -0,0 +1,10 @@
+import ContainerBlock from "../components/ContainerBlock";
+import Experience from "../components/Experience";
+
+export default function ExperiencePage() {
+	return (
+		<ContainerBlock>
+			<Experience />
+		</ContainerBlock>
+	)
+}
